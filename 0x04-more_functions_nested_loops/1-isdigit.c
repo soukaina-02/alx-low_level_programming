e6415a823062db88855474760c5f7117c511d284
garda <gardasoukaina@gmail.com>
2023-12-21 11:16:02 -0800

    hgfv

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,25 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
-    return 0;
 
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= '48' && c <= '57')
 		return (1);
 	else
 		return (0);
-}}
+}
