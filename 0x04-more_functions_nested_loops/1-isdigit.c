913ace9a3a1453fc5d375fc0651d42b544460112
garda <gardasoukaina@gmail.com>
2023-12-21 11:05:56 -0800

    hgfv

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -14,12 +14,12 @@ int main(void)
     printf("%c: %d\n", c, _isdigit(c));
     c = 'a';
     printf("%c: %d\n", c, _isdigit(c));
-    return (0);
-}
+    return 0;
+
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
-}
+}}
